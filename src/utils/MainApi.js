b6e30af723893f8e360d621329db00b5d9e72bbc
Nikita Sukhov <veyron-nike@yandex.ru>
2023-10-22 21:14:40 +0300

    настроены запросы на http://api.movies-explorer.sukhov-nikita.ru

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-  url: "https://api.watari.nomoredomains.xyz",
+  url: "http://api.movies-explorer.sukhov-nikita.ru",
   headers: {
     "content-type": "application/json",
   },
